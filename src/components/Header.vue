549220932fecb54fe3c99a7bf713e43eee4ef9b5
Dimitris Kapoulas <dimkapoulas@hotmail.com>
2021-03-09 15:38:15 +0200

    Project Finished

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @click-btn="$emit('click-btn')" 
+        :text="addTaskText ? 'Close' : 'Add Task'" 
+        :color="addTaskText ? 'red' : 'green'"/>
+    </header>
+</template>
+
+
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        addTaskText: Boolean
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: cente;
+    margin-bottom: 20px;
+}
+</style>
